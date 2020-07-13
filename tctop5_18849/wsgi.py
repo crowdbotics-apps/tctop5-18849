37a29337afeb645bd98e9d066ad4a9eef45ba22c
Crowdbotics <team@crowdbotics.com>
2020-07-13 20:23:17 +0000

    Update models

--- tctop5_18849/wsgi.py
+++ tctop5_18849/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tctop5_18849.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tctop5_18849.settings")
 
 application = get_wsgi_application()
